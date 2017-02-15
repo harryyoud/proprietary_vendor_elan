57f805c9054e531bc63c378762e1394dc071f656
Ziyan <jaraidaniel@gmail.com>
2017-02-15 20:38:49 +0000

    grouper: Reorganise structure

---
+++ grouper/grouper-vendor-blobs.mk
@@ -0,0 +1,4 @@
+VENDOR_FOLDER := vendor/elan/grouper
+
+PRODUCT_COPY_FILES += \
+	$(VENDOR_FOLDER)/proprietary/vendor/firmware/touch_fw.ekt:system/vendor/firmware/touch_fw.ekt
---
+++ grouper/grouper-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/elan/grouper/grouper-vendor-blobs.mk)
